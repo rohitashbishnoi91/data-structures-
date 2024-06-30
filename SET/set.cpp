4d88b1c92fd95719dd130840d3d7eb89eecd71e7
rohitashbishnoi91 <123815174+rohitashbishnoi91@users.noreply.github.com>
2024-06-30 18:30:38 +0000

    implementation of Disjoint Data sructutre

---
+++ SET/set.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+class DisJointSet{
+    vector<int> rank,parent;
+    public:
+    DisJointSet(int n){
+       rank.resize(n+1,0);
+       for(int i=0;i<=n;i++){
+        parent[i]=i;
+       }
+    }
+
+    int findP(int node){
+        if(node==parent[node]){
+            return node;
+        }
+        return parent[node]=findP(parent[node]);
+    }
+
+    void UnionByRank(int u,int v){
+        int ul_u=findP(u);
+        int ul_v=findP(v);
+
+        if(ul_u==ul_v) return;
+        if(rank[ul_u]<rank[ul_v]){
+            parent[ul_u]=ul_v;
+        }
+        else if(rank[ul_u]>rank[ul_v]){
+            parent[ul_v]=ul_u;
+        }
+        else{
+            parent[ul_u]=ul_v;
+            rank[ul_v]++;
+        }
+    }
+};
+int main(){
+    DisJointSet ds(7);
+    ds.UnionByRank(1,2);
+    ds.UnionByRank(2,3);
+    ds.UnionByRank(4,5);
+    ds.UnionByRank(6,7);
+    ds.UnionByRank(5,6);
+    if(ds.findP(3)==ds.findP(7)){
+        cout<<"same component"<<endl;
+    }
+    else{
+        cout<<"different componenrt"<<endl;
+    }
+    ds.UnionByRank(3,7);
+    return 0;
+}
