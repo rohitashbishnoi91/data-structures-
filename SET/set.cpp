310b67902c39f89bf93d20f3b26480240b0c0dc1
rohitashbishnoi91 <123815174+rohitashbishnoi91@users.noreply.github.com>
2024-07-04 22:54:08 +0530

    Update set.cpp

--- SET/set.cpp
+++ SET/set.cpp
@@ -1,39 +1,43 @@
 #include<bits/stdc++.h>
 using namespace std;
-class DisJointSet{
-    vector<int> rank,parent;
+class Solution
+{   
+    private:
+    class DisJointSet{
+        vector<int> rank, parent;
     public:
-    DisJointSet(int n){
-       rank.resize(n+1,0);
-       for(int i=0;i<=n;i++){
-        parent[i]=i;
-       }
-    }
+        DisJointSet(int n){
+            rank.resize(n+1, 0);
+            parent.resize(n+1);
+            for(int i = 0; i <= n; i++){
+                parent[i] = i;
+            }
+        }
 
-    int findP(int node){
-        if(node==parent[node]){
-            return node;
+        int findP(int node){
+            if(node == parent[node]){
+                return node;
+            }
+            return parent[node] = findP(parent[node]);
         }
-        return parent[node]=findP(parent[node]);
-    }
 
-    void UnionByRank(int u,int v){
-        int ul_u=findP(u);
-        int ul_v=findP(v);
+        void UnionByRank(int u, int v){
+            int ul_u = findP(u);
+            int ul_v = findP(v);
 
-        if(ul_u==ul_v) return;
-        if(rank[ul_u]<rank[ul_v]){
-            parent[ul_u]=ul_v;
+            if(ul_u == ul_v) return;
+            if(rank[ul_u] < rank[ul_v]){
+                parent[ul_u] = ul_v;
+            }
+            else if(rank[ul_u] > rank[ul_v]){
+                parent[ul_v] = ul_u;
+            }
+            else{
+                parent[ul_u] = ul_v;
+                rank[ul_v]++;
+            }
         }
-        else if(rank[ul_u]>rank[ul_v]){
-            parent[ul_v]=ul_u;
-        }
-        else{
-            parent[ul_u]=ul_v;
-            rank[ul_v]++;
-        }
-    }
-};
+    };
 int main(){
     DisJointSet ds(7);
     ds.UnionByRank(1,2);
